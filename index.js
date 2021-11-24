81f468ed6aefb0846f0896fbd86469454ed5a5f1
Haider Ali <malikhaider0567@gmail.com>
2021-11-24 14:54:31 +0530

    Completed the project

--- index.js
+++ index.js
@@ -15,22 +15,41 @@ searchBtn.addEventListener("click", function (e) {
 			`https://api.dictionaryapi.dev/api/v2/entries/en/${inp.value}`,
 			true
 		);
-		xhr.onprogress = function () { 
-			console.log("On Progress");
-		};
+
+		// xhr.onprogress = function () {
+		// 	console.log("On Progress");
+		// };
 
 		xhr.onload = function () {
 			if (this.status == 200) {
 				let res = this.response;
 				let json = JSON.parse(res);
 				let word = json[0];
+				let str = `<div class="card" style="width: 100%; text-align: left !important; background-color: rgb(243, 243, 243) !important;
+        border: none !important;">
+                    <div class="card-body">
+                      <h5 class="card-title" style="font-weight: bolder">${inp.value}</h5>`;
 				word.meanings.forEach((element) => {
-					console.log(element.partOfSpeech);
+					let initials = "as ";
+					if (
+						element.partOfSpeech.charAt(0) === "a" ||
+						element.partOfSpeech.charAt(0) === "e" ||
+						element.partOfSpeech.charAt(0) === "i" ||
+						element.partOfSpeech.charAt(0) === "o" ||
+						element.partOfSpeech.charAt(0) === "u"
+					)
+						initials += "an ";
+					else initials += "a ";
+					str += `<div><p style="text-decoration: underline;"><em>${initials}<strong>${element.partOfSpeech}</strong></em></p>`;
 					element.definitions.forEach((ele) => {
-						console.log(ele.definition);
+						str += `<p style="margin:0; padding:0">${ele.definition}</p>`;
+						str += `<p><strong>eg.</strong> ${ele.example}</p>`;
 					});
+					str += `</div>`;
 				});
-				console.log(word);
+				str += `</div></div>`;
+				word_div.innerHTML = str;
+				inp.value = "";
 			} else console.log("Some Error Occured");
 		};
 
